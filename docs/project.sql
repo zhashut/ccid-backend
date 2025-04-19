00e97ac7737c79cc8f587554269345e98c283643
zhashut <zhashut@gmail.com>
2025-04-19 08:24:30 +0800

    feat: 补充建表 SQL

---
+++ docs/project.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `data_property`
+(
+    `id`              bigint PRIMARY KEY AUTO_INCREMENT COMMENT '数据ID',
+    `type`            VARCHAR(255) NOT NULL COMMENT '数据类型（专利/论文/软件著作权）',
+    `title`           VARCHAR(255) NOT NULL COMMENT '标题',
+    `source`          VARCHAR(255) NOT NULL COMMENT '来源',
+    `collect_time`    DATE NULL COMMENT '采集时间',
+    `status`          TINYINT      NOT NULL DEFAULT 1 COMMENT '状态（1: 进行中, 2: 已完成）',
+    `patent_id`       VARCHAR(255) NOT NULL COMMENT '专利号',
+    `applier`         VARCHAR(255) NOT NULL COMMENT '申请人',
+    `apply_time`      DATE         NOT NULL COMMENT '申请日期',
+    `technical_field` VARCHAR(255) COMMENT '技术领域（如机器学习/计算机视觉）'
+) COMMENT '知识产权信息表';
+
+CREATE TABLE `data_task`
+(
+    `id`      bigint PRIMARY KEY AUTO_INCREMENT COMMENT '任务ID',
+    `name`    VARCHAR(255) NOT NULL COMMENT '任务名称（如专利数据清洗）',
+    `type`    VARCHAR(255) NOT NULL COMMENT '数据类型（专利/论文）',
+    `manager` VARCHAR(255) NOT NULL COMMENT '负责人',
+    `status`  TINYINT      NOT NULL DEFAULT 1 COMMENT '状态（1: 进行中, 2: 已完成）'
+) COMMENT '数据任务表';
+
+CREATE TABLE `data_report`
+(
+    `id`          bigint PRIMARY KEY AUTO_INCREMENT COMMENT '报告ID',
+    `name`        VARCHAR(255) NOT NULL COMMENT '报告名称',
+    `type`        VARCHAR(255) NOT NULL COMMENT '报告类型（专利分析/技术分析/市场分析）',
+    `status`      TINYINT      NOT NULL DEFAULT 1 COMMENT '状态（1: 查看, 2: 编辑，3: 下载）',
+    `create_time` DATE         NOT NULL COMMENT '生成时间'
+) COMMENT '数据报告表';
